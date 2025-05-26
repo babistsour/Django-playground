405f22919e166fdde974be855732bd05d664f976
babis <skoulikios@gmail.com>
2025-05-26 21:27:29 +0300

    Organize the code

--- products/models.py
+++ products/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='products/')
     second_image = models.ImageField(upload_to="products/", blank=True, null=True)
+    categories = models.ManyToManyField(Category, related_name='products')
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Product
+from .models import Category
 # Create your views here.
 
 def Product_list(request):
   products = Product.objects.all()
-  return render(request, 'products/product_list.html', {'products':products})
+  categories = Category.objects.all()
+  return render(request, 'products/product_list.html', {
+    'products':products,
+    'categories':categories
+    })
